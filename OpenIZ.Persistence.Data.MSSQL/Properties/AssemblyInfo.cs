216922e4fb663e6be3736ee47ef732d17dc79f95
jf03cg <justin@fyfesoftware.ca>
2015-12-31 10:46:27 -0500

    Added persistence and core projects. Integrated the MEDIC SVC Core

---
+++ OpenIZ.Persistence.Data.MSSQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenIZ.Persistence.Data.MSSQL")]
+[assembly: AssemblyDescription("OpenImmunize Microsoft SQL Server Data Persistence")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenImmunize Community")]
+[assembly: AssemblyProduct("OpenImmunize")]
+[assembly: AssemblyCopyright("Copyright ©  2015, Mohawk College of Applied Arts and Technology")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4e96db4-d4fa-4703-a3fd-5d9c30dfbb1e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
