3f35c60a9d93d65f03e6cd82118569295867e0ab
devbot9000 <devbot@devbot.com>
2016-02-10 16:50:31 -0500

    Added OAuth ACS

---
+++ OpenIZ.Authentication.AccessControlService/Wcf/IOAuthTokenContract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIZ.Authentication.AccessControlService.Wcf
+{
+    /// <summary>
+    /// OAuth2.0 Contract
+    /// </summary>
+    [ServiceContract]
+    public interface IOAuthTokenContract 
+    {
+
+        /// <summary>
+        /// Represents a token request
+        /// </summary>
+        [WebInvoke(UriTemplate = "/oauth20_token", Method = "POST")]
+        Stream Token();
+
+    }
+}
---
+++ OpenIZ.Authentication.AccessControlService/Wcf/OAuthTokenBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIZ.Authentication.AccessControlService.Wcf
+{
+    /// <summary>
+    /// OAuth Token Service
+    /// </summary>
+    public class OAuthTokenBehavior : IOAuthTokenContract
+    {
+        /// <summary>
+        /// OAuth Token Service
+        /// </summary>
+        public Stream Token()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
