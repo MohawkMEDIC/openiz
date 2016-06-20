d1a845b44578cb95a0747c092b8860cfaff47489
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2016-06-20 16:01:08 -0400

    I really dislike GIT randomly not adding my files.

---
+++ OpenIZ.Persistence.Data.MSSQL.Test/Services/PersonPersistenceServiceTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenIZ.Core.Model.Constants;
+using OpenIZ.Core.Model.DataTypes;
+using OpenIZ.Core.Model.Entities;
+using OpenIZ.Core.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIZ.Persistence.Data.MSSQL.Test.Services
+{
+    /// <summary>
+    /// Person persistence test
+    /// </summary>
+    [TestClass]
+    public class PersonPersistenceServiceTest : PersistenceTest<Person>
+    {
+
+
+        private static IPrincipal s_authorization;
+
+        [ClassInitialize]
+        public static void ClassSetup(TestContext context)
+        {
+            AppDomain.CurrentDomain.SetData(
+                           "DataDirectory",
+                           Path.Combine(context.TestDeploymentDir, string.Empty));
+            s_authorization = AuthenticationContext.SystemPrincipal;
+
+        }
+
+        /// <summary>
+        /// Test the persistence of a person
+        /// </summary>
+        [TestMethod]
+        public void TestPersistPerson()
+        {
+
+            Person p = new Person()
+            {
+                StatusConceptKey = StatusKeys.Active,
+                Names = new List<EntityName>()
+                {
+                    new EntityName(NameUseKeys.Legal, "Smith", "Johnny")
+                },
+                Addresses = new List<EntityAddress>()
+                {
+                    new EntityAddress(AddressUseKeys.HomeAddress, "123 Main Street West", "Hamilton", "ON", "CA", "L8K5N2")
+                },
+                    Identifiers = new List<EntityIdentifier>()
+                {
+                    new EntityIdentifier(new AssigningAuthority() { Name = "OHIPCARD", DomainName = "OHIPCARD", Oid = "1.2.3.4.5.6" }, "12343120423")
+                },
+                    Telecoms = new List<EntityTelecomAddress>()
+                {
+                    new EntityTelecomAddress(AddressUseKeys.WorkPlace, "mailto:bob@joe.com")
+                },
+                    Tags = new List<EntityTag>()
+                {
+                    new EntityTag("hasBirthCertificate", "true")
+                },
+                    Notes = new List<EntityNote>()
+                {
+                    new EntityNote(Guid.Empty, "Johnny is really allergic to eggs!")
+                    {
+                        Author = new Person()
+                    }
+                },
+                DateOfBirth = new DateTime(1984, 03, 22),
+                DateOfBirthPrecision = DatePrecision.Day
+            };
+            
+            var afterInsert = base.DoTestInsert(p, s_authorization);
+            Assert.AreEqual(DatePrecision.Day, afterInsert.DateOfBirthPrecision);
+            Assert.AreEqual(new DateTime(1984, 03, 22), afterInsert.DateOfBirth);
+            Assert.AreEqual(1, p.Names.Count);
+            Assert.AreEqual(1, p.Addresses.Count);
+            Assert.AreEqual(1, p.Identifiers.Count);
+            Assert.AreEqual(1, p.Telecoms.Count);
+            Assert.AreEqual(1, p.Tags.Count);
+            Assert.AreEqual(1, p.Notes.Count);
+            Assert.AreEqual(EntityClassKeys.Person, p.ClassConceptKey);
+            Assert.AreEqual(DeterminerKeys.Specific, p.DeterminerConceptKey);
+            Assert.AreEqual(StatusKeys.Active, p.StatusConceptKey);
+        }
+    }
+}
