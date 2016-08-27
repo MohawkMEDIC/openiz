7104d951f1b90c1697bc41a6fa35764cafac5a3b
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-08-27 14:39:41 -0400

    starting basic RISI shell

---
+++ OpenIZ.Messaging.RISI.Client/RisiServiceClient.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Copyright 2015-2016 Mohawk College of Applied Arts and Technology
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: Nityan
+ * Date: 2016-8-27
+ */
+
+using OpenIZ.Core.Interop.Clients;
+using System;
+using OpenIZ.Core.Http;
+
+namespace OpenIZ.Messaging.RISI.Client
+{
+	/// <summary>
+	/// Represents a RISI service client for interfacing with the RISI engine.
+	/// </summary>
+	public class RisiServiceClient : ServiceClientBase, IDisposable
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RisiServiceClient"/> class
+		/// with a specified <see cref="IRestClient"/> instance.
+		/// </summary>
+		/// <param name="restClient">The REST client instance.</param>
+		public RisiServiceClient(IRestClient restClient) : base(restClient)
+		{
+		}
+
+		#region IDisposable Support
+
+		private bool disposedValue = false; // To detect redundant calls
+
+		/// <summary>
+		/// Dispose of any managed resources.
+		/// </summary>
+		/// <param name="disposing">Whether the current invocation is disposing.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					this.Client?.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~RisiServiceClient() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		/// <summary>
+		/// Dispose of any managed resources.
+		/// </summary>
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
---
+++ OpenIZ.Messaging.RISI/RisiMessageHandler.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * Copyright 2015-2016 Mohawk College of Applied Arts and Technology
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: Nityan
+ * Date: 2016-8-27
+ */
+
+using MARC.HI.EHRS.SVC.Core;
+using MARC.HI.EHRS.SVC.Core.Services;
+using OpenIZ.Core.Wcf.Behavior;
+using OpenIZ.Messaging.RISI.Configuration;
+using OpenIZ.Messaging.RISI.Wcf;
+using System;
+using System.Diagnostics;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace OpenIZ.Messaging.RISI
+{
+	/// <summary>
+	/// Represents a message handler for reporting services.
+	/// </summary>
+	public class RisiMessageHandler : IDaemonService
+	{
+		/// <summary>
+		/// The internal reference to the trace source.
+		/// </summary>
+		private TraceSource m_traceSource = new TraceSource("OpenIZ.Messaging.RISI");
+
+		/// <summary>
+		/// The internal reference to the configuration.
+		/// </summary>
+		private RisiConfiguration m_configuration = ApplicationContext.Current.GetService<IConfigurationManager>().GetSection("openiz.messaging.risi") as RisiConfiguration;
+
+		/// <summary>
+		/// The internal reference to the web host.
+		/// </summary>
+		private WebServiceHost m_webHost;
+
+		/// <summary>
+		/// Gets the running state of the message handler.
+		/// </summary>
+		public bool IsRunning
+		{
+			get
+			{
+				return this.m_webHost?.State == System.ServiceModel.CommunicationState.Opened;
+			}
+		}
+
+		/// <summary>
+		/// Fired when the object is starting up.
+		/// </summary>
+		public event EventHandler Started;
+
+		/// <summary>
+		/// Fired when the object is starting.
+		/// </summary>
+		public event EventHandler Starting;
+
+		/// <summary>
+		/// Fired when the service has stopped.
+		/// </summary>
+		public event EventHandler Stopped;
+
+		/// <summary>
+		/// Fired when the service is stopping.
+		/// </summary>
+		public event EventHandler Stopping;
+
+		/// <summary>
+		/// Starts the service. Returns true if the service started successfully.
+		/// </summary>
+		/// <returns>Returns true if the service started successfully.</returns>
+		public bool Start()
+		{
+			try
+			{
+				this.Starting?.Invoke(this, EventArgs.Empty);
+
+				this.m_webHost = new WebServiceHost(typeof(RisiBehavior));
+
+				foreach (ServiceEndpoint endpoint in this.m_webHost.Description.Endpoints)
+				{
+					this.m_traceSource.TraceInformation("Starting RISI on {0}...", endpoint.Address);
+					endpoint.EndpointBehaviors.Add(new WcfErrorEndpointBehavior());
+				}
+
+				// Start the webhost
+				this.m_webHost.Open();
+
+				this.Started?.Invoke(this, EventArgs.Empty);
+				return true;
+			}
+			catch (Exception e)
+			{
+				this.m_traceSource.TraceEvent(TraceEventType.Error, e.HResult, e.ToString());
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Stops the service. Returns true if the service stopped successfully.
+		/// </summary>
+		/// <returns>Returns true if the service stopped successfully.</returns>
+		public bool Stop()
+		{
+			this.Stopping?.Invoke(this, EventArgs.Empty);
+
+			if (this.m_webHost != null)
+			{
+				this.m_webHost.Close();
+				this.m_webHost = null;
+			}
+
+			this.Stopped?.Invoke(this, EventArgs.Empty);
+
+			return true;
+		}
+	}
+}
