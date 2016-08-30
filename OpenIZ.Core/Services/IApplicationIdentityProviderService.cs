9c320cfd1cb9ac813ff50f8726d9c682da52e1cd
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-08-30 00:34:11 -0400

    automated code cleanup of the services

--- OpenIZ.Core/Services/IApplicationIdentityProviderService.cs
+++ OpenIZ.Core/Services/IApplicationIdentityProviderService.cs
@@ -1,56 +1,52 @@
 ﻿/*
  * Copyright 2015-2016 Mohawk College of Applied Arts and Technology
  *
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: justi
  * Date: 2016-7-7
  */
+
 using MARC.HI.EHRS.SVC.Core.Event;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenIZ.Core.Services
 {
-    /// <summary>
-    /// Represents a service which retrieves IPrincipal objects for applications
-    /// </summary>
-    public interface IApplicationIdentityProviderService 
-    {
+	/// <summary>
+	/// Represents a service which retrieves IPrincipal objects for applications
+	/// </summary>
+	public interface IApplicationIdentityProviderService
+	{
+		/// <summary>
+		/// Fired after an authentication request has been made
+		/// </summary>
+		event EventHandler<AuthenticatedEventArgs> Authenticated;
 
-        /// <summary>
-        /// Fired prior to an authentication request being made
-        /// </summary>
-        event EventHandler<AuthenticatingEventArgs> Authenticating;
+		/// <summary>
+		/// Fired prior to an authentication request being made
+		/// </summary>
+		event EventHandler<AuthenticatingEventArgs> Authenticating;
 
-        /// <summary>
-        /// Fired after an authentication request has been made
-        /// </summary>
-        event EventHandler<AuthenticatedEventArgs> Authenticated;
+		/// <summary>
+		/// Authenticate the application identity
+		/// </summary>
+		IPrincipal Authenticate(String applicationId, String applicationSecret);
 
-        /// <summary>
-        /// Authenticate the application identity
-        /// </summary>
-        IPrincipal Authenticate(String applicationId, String applicationSecret);
-
-        /// <summary>
-        /// Gets the specified identity
-        /// </summary>
-        IIdentity GetIdentity(string name);
-    }
+		/// <summary>
+		/// Gets the specified identity
+		/// </summary>
+		IIdentity GetIdentity(string name);
+	}
 }
--- OpenIZ.Core/Services/Impl/LocalMetadataRepositoryService.cs
+++ OpenIZ.Core/Services/Impl/LocalMetadataRepositoryService.cs
@@ -1,58 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MARC.HI.EHRS.SVC.Core;
+using MARC.HI.EHRS.SVC.Core.Services;
 using OpenIZ.Core.Model;
 using OpenIZ.Core.Model.DataTypes;
-using MARC.HI.EHRS.SVC.Core.Services;
-using MARC.HI.EHRS.SVC.Core;
 using OpenIZ.Core.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace OpenIZ.Core.Services.Impl
 {
-    /// <summary>
-    /// Represents a local metadata repository service
-    /// </summary>
-    public class LocalMetadataRepositoryService : IMetadataRepositoryService
-    {
-        /// <summary>
-        /// Find an assigning authority
-        /// </summary>
-        public IEnumerable<IdentifiedData> FindAssigningAuthority(Expression<Func<AssigningAuthority, bool>> query)
-        {
-            var persistenceService = ApplicationContext.Current.GetService<IDataPersistenceService<AssigningAuthority>>();
-            if (persistenceService == null)
-                throw new InvalidOperationException("No concept persistence service found");
+	/// <summary>
+	/// Represents a local metadata repository service
+	/// </summary>
+	public class LocalMetadataRepositoryService : IMetadataRepositoryService
+	{
+		/// <summary>
+		/// Find an assigning authority
+		/// </summary>
+		public IEnumerable<IdentifiedData> FindAssigningAuthority(Expression<Func<AssigningAuthority, bool>> query)
+		{
+			var persistenceService = ApplicationContext.Current.GetService<IDataPersistenceService<AssigningAuthority>>();
+			if (persistenceService == null)
+				throw new InvalidOperationException("No concept persistence service found");
 
-            int totalResults = 0;
-            return persistenceService.Query(query, 0, 100, AuthenticationContext.Current.Principal, out totalResults);
+			int totalResults = 0;
+			return persistenceService.Query(query, 0, 100, AuthenticationContext.Current.Principal, out totalResults);
+		}
 
-        }
+		/// <summary>
+		/// Find assigning authority
+		/// </summary>
+		public IEnumerable<IdentifiedData> FindAssigningAuthority(Expression<Func<AssigningAuthority, bool>> query, int offset, int count, out int totalCount)
+		{
+			var persistenceService = ApplicationContext.Current.GetService<IDataPersistenceService<AssigningAuthority>>();
+			if (persistenceService == null)
+				throw new InvalidOperationException("No concept persistence service found");
 
-        /// <summary>
-        /// Find assigning authority 
-        /// </summary>
-        public IEnumerable<IdentifiedData> FindAssigningAuthority(Expression<Func<AssigningAuthority, bool>> query, int offset, int count, out int totalCount)
-        {
-            var persistenceService = ApplicationContext.Current.GetService<IDataPersistenceService<AssigningAuthority>>();
-            if (persistenceService == null)
-                throw new InvalidOperationException("No concept persistence service found");
+			return persistenceService.Query(query, offset, count, AuthenticationContext.Current.Principal, out totalCount);
+		}
 
-            return persistenceService.Query(query, offset, count, AuthenticationContext.Current.Principal, out totalCount);
-        }
+		/// <summary>
+		/// Get the assigning authority
+		/// </summary>
+		public IdentifiedData GetAssigningAuthority(Guid id)
+		{
+			var persistenceService = ApplicationContext.Current.GetService<IDataPersistenceService<AssigningAuthority>>();
+			if (persistenceService == null)
+				throw new InvalidOperationException("No concept persistence service found");
 
-        /// <summary>
-        /// Get the assigning authority
-        /// </summary>
-        public IdentifiedData GetAssigningAuthority(Guid id)
-        {
-            var persistenceService = ApplicationContext.Current.GetService<IDataPersistenceService<AssigningAuthority>>();
-            if (persistenceService == null)
-                throw new InvalidOperationException("No concept persistence service found");
-
-            return persistenceService.Get<Guid>(new MARC.HI.EHRS.SVC.Core.Data.Identifier<Guid>(id), AuthenticationContext.Current.Principal, false);
-        }
-    }
+			return persistenceService.Get<Guid>(new MARC.HI.EHRS.SVC.Core.Data.Identifier<Guid>(id), AuthenticationContext.Current.Principal, false);
+		}
+	}
 }
