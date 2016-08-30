3dcd82f23b11a6e5643532e2771935a6a6a28b94
Justin Fyfe <justin@fyfesoftware.ca>
2016-08-30 13:09:38 -0400

    Patient get on optimized binding returns original model

--- OpenIZ.Messaging.IMSI.Client/ImsiServiceClient.cs
+++ OpenIZ.Messaging.IMSI.Client/ImsiServiceClient.cs
@@ -99,7 +99,9 @@ namespace OpenIZ.Messaging.IMSI.Client
 			// Optimize?
 			if (this.Client.Description.Binding.Optimize)
 			{
-				return this.Client.Get<Bundle>(url.ToString(), new KeyValuePair<string, object>("_bundle", "true"));
+				var bundle = this.Client.Get<Bundle>(url.ToString(), new KeyValuePair<string, object>("_bundle", "true"));
+                bundle.Reconstitute();
+                return bundle.Entry as TModel;
 			}
 			else
 			{
