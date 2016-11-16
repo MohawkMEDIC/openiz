779620913ad3c70fa892739c38297890cf1ef94b
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-11-15 23:57:25 -0500

    added a business rules core service handler

---
+++ OpenIZ.BusinessRules.Core/Configuration/BusinessRulesCoreConfiguration.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright 2015-2016 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: Nityan
+ * Date: 2016-11-15
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIZ.BusinessRules.Core.Configuration
+{
+	/// <summary>
+	/// Represents a business rules core configuration.
+	/// </summary>
+	public class BusinessRulesCoreConfiguration
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BusinessRulesCoreConfiguration"/> class.
+		/// </summary>
+		public BusinessRulesCoreConfiguration()
+		{
+
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BusinessRulesCoreConfiguration"/> class
+		/// with a specific directory.
+		/// </summary>
+		/// <param name="path">The business rules core configuration directory.</param>
+		public BusinessRulesCoreConfiguration(string path)
+		{
+			this.Path = path;
+		}
+
+		/// <summary>
+		/// Gets or sets the path of the business rules core configuration.
+		/// </summary>
+		public string Path { get; set; }
+	}
+}
---
+++ OpenIZ.BusinessRules.Core/Configuration/ConfigurationSectionHandler.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Copyright 2015-2016 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: Nityan
+ * Date: 2016-11-15
+ */
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OpenIZ.BusinessRules.Core.Configuration
+{
+	/// <summary>
+	/// Represents a configuration section handler for a business rules configuration.
+	/// </summary>
+	public class ConfigurationSectionHandler : IConfigurationSectionHandler
+	{
+		/// <summary>
+		/// Creates the configuration section.
+		/// </summary>
+		/// <param name="parent">The parent configuration section.</param>
+		/// <param name="configContext">The configuration context.</param>
+		/// <param name="section">The configuration section.</param>
+		/// <returns>Returns an instance of the configuration section.</returns>
+		public object Create(object parent, object configContext, XmlNode section)
+		{
+			var configuration = new BusinessRulesCoreConfiguration();
+			var directoryElement = section.SelectSingleNode("./*[local-name() = 'directory']") as XmlElement;
+
+			var path = directoryElement?.Attributes["path"]?.Value;
+
+			if (path == null)
+			{
+				throw new ConfigurationErrorsException("Missing directory", section);
+			}
+
+			if (!Directory.Exists(path))
+			{
+				throw new ConfigurationErrorsException($"Path {path} does not exist");
+			}
+
+			return new BusinessRulesCoreConfiguration(path);
+		}
+	}
+}
