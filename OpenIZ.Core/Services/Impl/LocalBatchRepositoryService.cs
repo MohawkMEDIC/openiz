e397450a5975c90d55c7625be77f1193b951e163
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-12-12 21:37:38 -0500

    cleaning up the bundle resource handler and added missing license headers

--- OpenIZ.Core/Services/Impl/LocalBatchRepositoryService.cs
+++ OpenIZ.Core/Services/Impl/LocalBatchRepositoryService.cs
@@ -6,97 +6,94 @@ using OpenIZ.Core.Model.Collection;
 using OpenIZ.Core.Model.Roles;
 using OpenIZ.Core.Security;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenIZ.Core.Services.Impl
 {
-    /// <summary>
-    /// Local batch repository service 
-    /// </summary>
-    public class LocalBatchRepositoryService : IBatchRepositoryService
-    {
-        /// <summary>
+	/// <summary>
+	/// Local batch repository service
+	/// </summary>
+	public class LocalBatchRepositoryService : IBatchRepositoryService
+	{
+		/// <summary>
 		/// Insert the bundle
 		/// </summary>
 		public Bundle Insert(Bundle data)
-        {
-            data = this.Validate(data);
-            var persistence = ApplicationContext.Current.GetService<IDataPersistenceService<Bundle>>();
-            if (persistence == null)
-                throw new InvalidOperationException("Missing persistence service");
-            var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
+		{
+			data = this.Validate(data);
+			var persistence = ApplicationContext.Current.GetService<IDataPersistenceService<Bundle>>();
+			if (persistence == null)
+				throw new InvalidOperationException("Missing persistence service");
+			var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
 
-            data = breService?.BeforeInsert(data) ?? data;
-            data = persistence.Insert(data, AuthenticationContext.Current.Principal, TransactionMode.Commit);
-            data = breService?.AfterInsert(data) ?? data;
+			data = breService?.BeforeInsert(data) ?? data;
+			data = persistence.Insert(data, AuthenticationContext.Current.Principal, TransactionMode.Commit);
+			data = breService?.AfterInsert(data) ?? data;
 
-            return data;
-        }
+			return data;
+		}
 
-        /// <summary>
-        /// Obsolete all the contents in the bundle
-        /// </summary>
-        public Bundle Obsolete(Bundle obsolete)
-        {
-            obsolete = this.Validate(obsolete);
-            var persistence = ApplicationContext.Current.GetService<IDataPersistenceService<Bundle>>();
-            if (persistence == null)
-                throw new InvalidOperationException("Missing persistence service");
-            var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
+		/// <summary>
+		/// Obsolete all the contents in the bundle
+		/// </summary>
+		public Bundle Obsolete(Bundle obsolete)
+		{
+			obsolete = this.Validate(obsolete);
+			var persistence = ApplicationContext.Current.GetService<IDataPersistenceService<Bundle>>();
+			if (persistence == null)
+				throw new InvalidOperationException("Missing persistence service");
+			var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
 
-            obsolete = breService?.BeforeObsolete(obsolete) ?? obsolete;
-            obsolete = persistence.Obsolete(obsolete, AuthenticationContext.Current.Principal, TransactionMode.Commit);
-            obsolete = breService?.AfterObsolete(obsolete) ?? obsolete;
+			obsolete = breService?.BeforeObsolete(obsolete) ?? obsolete;
+			obsolete = persistence.Obsolete(obsolete, AuthenticationContext.Current.Principal, TransactionMode.Commit);
+			obsolete = breService?.AfterObsolete(obsolete) ?? obsolete;
 
-            return obsolete;
-        }
+			return obsolete;
+		}
 
-        /// <summary>
-        /// Update the specified data in the bundle
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public Bundle Update(Bundle data)
-        {
-            data = this.Validate(data);
-            var persistence = ApplicationContext.Current.GetService<IDataPersistenceService<Bundle>>();
-            if (persistence == null)
-                throw new InvalidOperationException("Missing persistence service");
-            var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
+		/// <summary>
+		/// Update the specified data in the bundle
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public Bundle Update(Bundle data)
+		{
+			data = this.Validate(data);
+			var persistence = ApplicationContext.Current.GetService<IDataPersistenceService<Bundle>>();
+			if (persistence == null)
+				throw new InvalidOperationException("Missing persistence service");
+			var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
 
-            // Entry point
-            data = breService?.BeforeUpdate(data) ?? data;
-            data = persistence.Insert(data, AuthenticationContext.Current.Principal, TransactionMode.Commit);
-            data = breService?.AfterUpdate(data) ?? data;
-            return data;
-        }
+			// Entry point
+			data = breService?.BeforeUpdate(data) ?? data;
+			data = persistence.Insert(data, AuthenticationContext.Current.Principal, TransactionMode.Commit);
+			data = breService?.AfterUpdate(data) ?? data;
+			return data;
+		}
 
-        /// <summary>
-        /// Validate the bundle and its contents
-        /// </summary>
-        public Bundle Validate(Bundle bundle)
-        {
-            bundle = bundle.Clean() as Bundle;
+		/// <summary>
+		/// Validate the bundle and its contents
+		/// </summary>
+		public Bundle Validate(Bundle bundle)
+		{
+			bundle = bundle.Clean() as Bundle;
 
-            // BRE validation
-            var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
-            var issues = breService?.Validate(bundle);
-            if (issues?.Any(i => i.Priority == DetectedIssuePriorityType.Error) == true)
-                throw new DetectedIssueException(issues);
+			// BRE validation
+			var breService = ApplicationContext.Current.GetService<IBusinessRulesService<Bundle>>();
+			var issues = breService?.Validate(bundle);
+			if (issues?.Any(i => i.Priority == DetectedIssuePriorityType.Error) == true)
+				throw new DetectedIssueException(issues);
 
-            // Bundle items
-            for (int i = 0; i < bundle.Item.Count; i++)
-            {
-                var itm = bundle.Item[i];
-                if (itm is Patient)
-                    bundle.Item[i] = ApplicationContext.Current.GetService<IPatientRepositoryService>().Validate(itm as Patient);
-                else if (itm is Act)
-                    bundle.Item[i] = ApplicationContext.Current.GetService<IActRepositoryService>().Validate(itm as Act);
-            }
-            return bundle;
-        }
-    }
+			// Bundle items
+			for (int i = 0; i < bundle.Item.Count; i++)
+			{
+				var itm = bundle.Item[i];
+				if (itm is Patient)
+					bundle.Item[i] = ApplicationContext.Current.GetService<IPatientRepositoryService>().Validate(itm as Patient);
+				else if (itm is Act)
+					bundle.Item[i] = ApplicationContext.Current.GetService<IActRepositoryService>().Validate(itm as Act);
+			}
+			return bundle;
+		}
+	}
 }
--- OpenIZ.Messaging.IMSI/ResourceHandler/BundleResourceHandler.cs
+++ OpenIZ.Messaging.IMSI/ResourceHandler/BundleResourceHandler.cs
@@ -1,120 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MARC.HI.EHRS.SVC.Core;
 using OpenIZ.Core.Model;
-using OpenIZ.Core.Model.Query;
 using OpenIZ.Core.Model.Collection;
+using OpenIZ.Core.Model.Query;
 using OpenIZ.Core.Services;
-using MARC.HI.EHRS.SVC.Core;
+using System;
+using System.Collections.Generic;
 
 namespace OpenIZ.Messaging.IMSI.ResourceHandler
 {
-    /// <summary>
-    /// Represents a resource handler which is for the persistence of bundles
-    /// </summary>
-    public class BundleResourceHandler : IResourceHandler
-    {
-        /// <summary>
+	/// <summary>
+	/// Represents a resource handler which is for the persistence of bundles.
+	/// </summary>
+	public class BundleResourceHandler : IResourceHandler
+	{
+		/// <summary>
 		/// The internal reference to the <see cref="IBatchRepositoryService"/> instance.
 		/// </summary>
-		private IBatchRepositoryService m_repositoryService;
+		private IBatchRepositoryService repositoryService;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EntityResourceHandler"/> class.
-        /// </summary>
-        public BundleResourceHandler()
-        {
-            ApplicationContext.Current.Started += (o, e) => this.m_repositoryService = ApplicationContext.Current.GetService<IBatchRepositoryService>();
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BundleResourceHandler"/> class.
+		/// </summary>
+		public BundleResourceHandler()
+		{
+			ApplicationContext.Current.Started += (o, e) => this.repositoryService = ApplicationContext.Current.GetService<IBatchRepositoryService>();
+		}
 
-        /// <summary>
-        /// Gets the resource name
-        /// </summary>
-        public string ResourceName
-        {
-            get
-            {
-                return "Bundle";
-            }
-        }
+		/// <summary>
+		/// Gets the resource name which this resource handler handles.
+		/// </summary>
+		public string ResourceName => "Bundle";
 
-        /// <summary>
-        /// Gets the type which this handles
-        /// </summary>
-        public Type Type
-        {
-            get
-            {
-                return typeof(Bundle);
-            }
-        }
+		/// <summary>
+		/// Gets the type which this resource handler handles.
+		/// </summary>
+		public Type Type => typeof(Bundle);
 
-        /// <summary>
-        /// Create the specified bundle
-        /// </summary>
-        public IdentifiedData Create(IdentifiedData data, bool updateIfExists)
-        {
-            if (data == null)
-                throw new ArgumentNullException(nameof(data));
-            else if (updateIfExists)
-                throw new ArgumentOutOfRangeException(nameof(updateIfExists));
+		/// <summary>
+		/// Creates a bundle.
+		/// </summary>
+		/// <param name="data">The data to create.</param>
+		/// <param name="updateIfExists">Whether to update an existing entity.</param>
+		/// <returns>Returns the created bundle.</returns>
+		public IdentifiedData Create(IdentifiedData data, bool updateIfExists)
+		{
+			if (data == null)
+			{
+				throw new ArgumentNullException(nameof(data));
+			}
 
-            var bundle = data as Bundle;
-            if (bundle == null)
-                throw new ArgumentException("Bundle required", nameof(data));
-            bundle.Reconstitute();
-            // Submit
-            return this.m_repositoryService.Insert(bundle);
-        }
+			if (updateIfExists)
+			{
+				throw new ArgumentOutOfRangeException(nameof(updateIfExists));
+			}
 
-        /// <summary>
-        /// Gets the specified data
-        /// </summary>
-        public IdentifiedData Get(Guid id, Guid versionId)
-        {
-            throw new NotSupportedException();
-        }
+			var bundle = data as Bundle;
 
-        /// <summary>
-        /// Obsoletes the bundle
-        /// </summary>
-        public IdentifiedData Obsolete(Guid key)
-        {
-            throw new NotSupportedException();
-        }
+			if (bundle == null)
+			{
+				throw new ArgumentException("Bundle required", nameof(data));
+			}
 
-        /// <summary>
-        /// Query for bundle
-        /// </summary>
-        public IEnumerable<IdentifiedData> Query(NameValueCollection queryParameters)
-        {
-            throw new NotSupportedException();
-        }
+			bundle.Reconstitute();
 
-        /// <summary>
-        /// Query bundle
-        /// </summary>
-        public IEnumerable<IdentifiedData> Query(NameValueCollection queryParameters, int offset, int count, out int totalCount)
-        {
-            throw new NotSupportedException();
-        }
+			// Submit
+			return this.repositoryService.Insert(bundle);
+		}
 
-        /// <summary>
-        /// Update the bundle
-        /// </summary>
-        public IdentifiedData Update(IdentifiedData data)
-        {
-            if (data == null)
-                throw new ArgumentNullException(nameof(data));
+		/// <summary>
+		/// Gets the specified data
+		/// </summary>
+		public IdentifiedData Get(Guid id, Guid versionId)
+		{
+			throw new NotSupportedException();
+		}
 
-            var bundle = data as Bundle;
-            if (bundle == null)
-                throw new ArgumentException("Bundle required", nameof(data));
+		/// <summary>
+		/// Obsoletes the bundle
+		/// </summary>
+		public IdentifiedData Obsolete(Guid key)
+		{
+			throw new NotSupportedException();
+		}
 
-            // Submit
-            return this.m_repositoryService.Update(bundle);
-        }
-    }
+		/// <summary>
+		/// Query for bundle
+		/// </summary>
+		public IEnumerable<IdentifiedData> Query(NameValueCollection queryParameters)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Query bundle
+		/// </summary>
+		public IEnumerable<IdentifiedData> Query(NameValueCollection queryParameters, int offset, int count, out int totalCount)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Updates a specific bundle.
+		/// </summary>
+		/// <param name="data">The data to be updated.</param>
+		/// <returns>Returns the updated data.</returns>
+		public IdentifiedData Update(IdentifiedData data)
+		{
+			if (data == null)
+			{
+				throw new ArgumentNullException(nameof(data));
+			}
+
+			var bundle = data as Bundle;
+
+			if (bundle == null)
+			{
+				throw new ArgumentException("Bundle required", nameof(data));
+			}
+
+			// Submit
+			return this.repositoryService.Update(bundle);
+		}
+	}
 }
