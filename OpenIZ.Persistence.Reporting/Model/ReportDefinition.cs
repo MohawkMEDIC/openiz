62d885b496df7fed25d1e85e7525715f0439e705
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-01-06 23:08:26 -0500

    added basic report persistence

---
+++ OpenIZ.Persistence.Reporting/Model/ReportDefinition.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright 2015-2017 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: khannan
+ * Date: 2017-1-6
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OpenIZ.Persistence.Reporting.Model
+{
+	/// <summary>
+	/// Represents a report definition.
+	/// </summary>
+	[XmlRoot(nameof(ReportDefinition), Namespace = "http://openiz.org/risi")]
+	[XmlType(nameof(ReportDefinition), Namespace = "http://openiz.org/risi")]
+	public class ReportDefinition
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ReportDefinition"/> class.
+		/// </summary>
+		public ReportDefinition()
+		{
+			
+		}
+	}
+}
