345b7f0f56be185795760f69450120ca18837562
Justin Fyfe <justin@fyfesoftware.ca>
2017-03-13 02:53:05 -0400

    Added scumbag git missing file

---
+++ OpenIZ.OrmLite/Attributes/NotNullAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIZ.OrmLite.Attributes
+{
+    /// <summary>
+    /// Not null attribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NotNullAttribute : System.Attribute
+    {
+
+
+
+    }
+}
