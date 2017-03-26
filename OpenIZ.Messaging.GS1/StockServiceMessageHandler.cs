0275917e51f4f0d6ef122133aeef9b5a1d49f11e
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-03-26 02:26:34 -0400

    GS1 code cleanup and license headers

--- OpenIZ.Messaging.GS1/StockServiceMessageHandler.cs
+++ OpenIZ.Messaging.GS1/StockServiceMessageHandler.cs
@@ -1,103 +1,111 @@
-﻿using MARC.HI.EHRS.SVC.Core;
+﻿/*
+ * Copyright 2015-2017 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: fyfej
+ * Date: 2017-3-24
+ */
 using MARC.HI.EHRS.SVC.Core.Services;
 using OpenIZ.Messaging.GS1.Wcf;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.ServiceModel.Web;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenIZ.Messaging.GS1
 {
-    /// <summary>
-    /// Stock service message handler
-    /// </summary>
-    public class StockServiceMessageHandler : IMessageHandlerService
-    {
+	/// <summary>
+	/// Stock service message handler
+	/// </summary>
+	public class StockServiceMessageHandler : IMessageHandlerService
+	{
+		// IMSI Trace host
+		private readonly TraceSource traceSource = new TraceSource("OpenIZ.Messaging.GS1");
 
+		// web host
+		private WebServiceHost webHost;
 
-        // IMSI Trace host
-        private TraceSource m_traceSource = new TraceSource("OpenIZ.Messaging.StockService");
+		/// <summary>
+		/// Fired when the object is starting up
+		/// </summary>
+		public event EventHandler Started;
 
-        // web host
-        private WebServiceHost m_webHost;
+		/// <summary>
+		/// Fired when the object is starting
+		/// </summary>
+		public event EventHandler Starting;
 
-        /// <summary>
-        /// True if running
-        /// </summary>
-        public bool IsRunning
-        {
-            get
-            {
-                return this.m_webHost?.State == System.ServiceModel.CommunicationState.Opened;
-            }
-        }
+		/// <summary>
+		/// Fired when the service has stopped
+		/// </summary>
+		public event EventHandler Stopped;
 
-        /// <summary>
-        /// Fired when the object is starting up
-        /// </summary>
-        public event EventHandler Started;
-        /// <summary>
-        /// Fired when the object is starting
-        /// </summary>
-        public event EventHandler Starting;
-        /// <summary>
-        /// Fired when the service has stopped
-        /// </summary>
-        public event EventHandler Stopped;
-        /// <summary>
-        /// Fired when the service is stopping
-        /// </summary>
-        public event EventHandler Stopping;
+		/// <summary>
+		/// Fired when the service is stopping
+		/// </summary>
+		public event EventHandler Stopping;
 
-        /// <summary>
-        /// Start the service
-        /// </summary>
-        public bool Start()
-        {
-            try
-            {
-                this.Starting?.Invoke(this, EventArgs.Empty);
+		/// <summary>
+		/// True if running
+		/// </summary>
+		public bool IsRunning => this.webHost?.State == CommunicationState.Opened;
 
-                this.m_webHost = new WebServiceHost(typeof(StockServiceBehavior));
-                foreach (ServiceEndpoint endpoint in this.m_webHost.Description.Endpoints)
-                {
-                    this.m_traceSource.TraceInformation("Starting GS1 on {0}...", endpoint.Address);
-                }
-                // Start the webhost
-                this.m_webHost.Open();
+		/// <summary>
+		/// Start the service
+		/// </summary>
+		public bool Start()
+		{
+			try
+			{
+				this.Starting?.Invoke(this, EventArgs.Empty);
 
-                this.Started?.Invoke(this, EventArgs.Empty);
-                return true;
-            }
-            catch (Exception e)
-            {
-                this.m_traceSource.TraceEvent(TraceEventType.Error, e.HResult, e.ToString());
-                return false;
-            }
-        }
+				this.webHost = new WebServiceHost(typeof(StockServiceBehavior));
+				foreach (ServiceEndpoint endpoint in this.webHost.Description.Endpoints)
+				{
+					this.traceSource.TraceInformation("Starting GS1 on {0}...", endpoint.Address);
+				}
+				// Start the webhost
+				this.webHost.Open();
 
-        /// <summary>
-        /// Stop the IMSI service
-        /// </summary>
-        public bool Stop()
-        {
-            this.Stopping?.Invoke(this, EventArgs.Empty);
+				this.Started?.Invoke(this, EventArgs.Empty);
+				return true;
+			}
+			catch (Exception e)
+			{
+				this.traceSource.TraceEvent(TraceEventType.Error, e.HResult, e.ToString());
+				return false;
+			}
+		}
 
-            if (this.m_webHost != null)
-            {
-                this.m_webHost.Close();
-                this.m_webHost = null;
-            }
+		/// <summary>
+		/// Stop the IMSI service
+		/// </summary>
+		public bool Stop()
+		{
+			this.Stopping?.Invoke(this, EventArgs.Empty);
 
-            this.Stopped?.Invoke(this, EventArgs.Empty);
+			if (this.webHost != null)
+			{
+				this.webHost.Close();
+				this.webHost = null;
+			}
 
-            return true;
-        }
+			this.Stopped?.Invoke(this, EventArgs.Empty);
 
-    }
+			return true;
+		}
+	}
 }
--- OpenIZ.Messaging.GS1/Wcf/IStockService.cs
+++ OpenIZ.Messaging.GS1/Wcf/IStockService.cs
@@ -1,27 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Copyright 2015-2017 Mohawk College of Applied Arts and Technology
+ *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: fyfej
+ * Date: 2017-3-24
+ */
 using OpenIZ.Messaging.GS1.Model;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 
 namespace OpenIZ.Messaging.GS1.Wcf
 {
-    /// <summary>
-    /// Stock service request
-    /// </summary>
-    [ServiceContract(ConfigurationName = "GS1BMS", Name = "GS1BMS")]
-    [XmlSerializerFormat]
-    public interface IStockService
-    {
-
-        /// <summary>
-        /// Represents a request for issuance of an inventory report
-        /// </summary>
-        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, UriTemplate = "/inventoryReport")]
-        LogisticsInventoryReportMessageType IssueInventoryReportRequest(LogisticsInventoryReportRequestMessageType parameters);
-        
-    }
+	/// <summary>
+	/// Stock service request
+	/// </summary>
+	[ServiceContract(ConfigurationName = "GS1BMS", Name = "GS1BMS")]
+	[XmlSerializerFormat]
+	public interface IStockService
+	{
+		/// <summary>
+		/// Represents a request for issuance of an inventory report
+		/// </summary>
+		[WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, UriTemplate = "/inventoryReport")]
+		LogisticsInventoryReportMessageType IssueInventoryReportRequest(LogisticsInventoryReportRequestMessageType parameters);
+	}
 }
