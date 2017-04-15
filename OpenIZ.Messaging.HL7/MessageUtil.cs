28d087b367cce5e324042c5a059c1c88dd1d5694
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-04-15 03:14:40 -0400

    v2 mapping cleanup

--- OpenIZ.Messaging.HL7/MessageUtil.cs
+++ OpenIZ.Messaging.HL7/MessageUtil.cs
@@ -26,6 +26,7 @@ using NHapi.Base.Parser;
 using NHapi.Base.Util;
 using NHapi.Base.validation.impl;
 using NHapi.Model.V231.Datatype;
+using NHapi.Model.V231.Message;
 using NHapi.Model.V25.Segment;
 using OpenIZ.Core;
 using OpenIZ.Core.Model.Constants;
@@ -62,16 +63,6 @@ namespace OpenIZ.Messaging.HL7
 		private static TraceSource tracer = new TraceSource("OpenIZ.Messaging.HL7");
 
 		/// <summary>
-		/// The internal reference to the <see cref="IConceptRepositoryService"/> instance.
-		/// </summary>
-		private static IConceptRepositoryService conceptRepositoryService;
-
-		/// <summary>
-		/// The internal reference to the <see cref="ILocalizationService"/> instance.
-		/// </summary>
-		private static ILocalizationService localizationService;
-
-		/// <summary>
 		/// Converts an <see cref="XAD"/> address instance to an <see cref="EntityAddress"/> instance.
 		/// </summary>
 		/// <param name="address">The address to be converted.</param>
@@ -103,7 +94,7 @@ namespace OpenIZ.Messaging.HL7
 
 				if (!string.IsNullOrEmpty(addresses[i].AddressType.Value) && !string.IsNullOrWhiteSpace(addresses[i].AddressType.Value))
 				{
-					var concept = GetConcept(addresses[i].AddressType.Value, "2.16.840.1.113883.5.1");
+					var concept = GetConcept(addresses[i].AddressType.Value, "urn:oid:2.16.840.1.113883.5.1");
 
 					// TODO: cleanup
 					if (concept == null)
@@ -194,7 +185,42 @@ namespace OpenIZ.Messaging.HL7
 
 			if (!string.IsNullOrEmpty(id.UniversalIDType.Value) && id.UniversalIDType.Value != "ISO")
 			{
-				details.Add(new NotImplementedResultDetail(ResultDetailType.Warning, MessageUtil.localizationService.GetString("MSGW016"), null));
+				details.Add(new NotImplementedResultDetail(ResultDetailType.Warning, ApplicationContext.Current.GetLocaleString("MSGW016"), null));
+			}
+
+			return assigningAuthority;
+		}
+
+		/// <summary>
+		/// Converts an <see cref="NHapi.Model.V25.Datatype.HD"/> instance to an <see cref="AssigningAuthority"/> instance.
+		/// </summary>
+		/// <param name="id">The id value to be converted.</param>
+		/// <returns>Returns the converted assigning authority.</returns>
+		public static AssigningAuthority ConvertAssigningAuthority(NHapi.Model.V25.Datatype.HD id, List<IResultDetail> details)
+		{
+			var assigningAuthorityRepositoryService = ApplicationContext.Current.GetAssigningAuthorityService();
+
+			AssigningAuthority assigningAuthority = null;
+
+			if (id == null)
+			{
+				details.Add(new MandatoryElementMissingResultDetail(ResultDetailType.Error, null, null));
+				return assigningAuthority;
+			}
+
+			if (!string.IsNullOrEmpty(id.NamespaceID.Value))
+			{
+				assigningAuthority = assigningAuthorityRepositoryService.Find(a => a.Name == id.NamespaceID.Value).FirstOrDefault();
+			}
+
+			if (!string.IsNullOrEmpty(id.UniversalID.Value))
+			{
+				assigningAuthority = assigningAuthorityRepositoryService.Find(a => a.Oid == id.UniversalID.Value).FirstOrDefault();
+			}
+
+			if (!string.IsNullOrEmpty(id.UniversalIDType.Value) && id.UniversalIDType.Value != "ISO")
+			{
+				details.Add(new NotImplementedResultDetail(ResultDetailType.Warning, ApplicationContext.Current.GetLocaleString("MSGW016"), null));
 			}
 
 			return assigningAuthority;
@@ -217,16 +243,16 @@ namespace OpenIZ.Messaging.HL7
 		/// <returns>Returns a list of entity identifiers.</returns>
 		public static IEnumerable<EntityIdentifier> ConvertIdentifiers(CX[] identifiers)
 		{
-			List<EntityIdentifier> entityIdentifiers = new List<EntityIdentifier>();
+			var entityIdentifiers = new List<EntityIdentifier>();
 
 			if (identifiers.Length == 0)
 			{
 				return entityIdentifiers.AsEnumerable();
 			}
 
-			for (int i = 0; i < identifiers.Length; i++)
+			for (var i = 0; i < identifiers.Length; i++)
 			{
-				EntityIdentifier entityIdentifier = new EntityIdentifier();
+				var entityIdentifier = new EntityIdentifier();
 
 				var cx = identifiers[i];
 
@@ -241,14 +267,14 @@ namespace OpenIZ.Messaging.HL7
 
 				if (assigningAuthority == null)
 				{
-					MessageUtil.tracer.TraceEvent(TraceEventType.Warning, 0, $"Assigning authority OID not found: {cx.AssigningAuthority.UniversalID.Value}");
+					tracer.TraceEvent(TraceEventType.Warning, 0, $"Assigning authority OID not found: {cx.AssigningAuthority.UniversalID.Value}");
 				}
 				else
 				{
 #if DEBUG
-					MessageUtil.tracer.TraceEvent(TraceEventType.Information, 0, $"Adding {cx.ID.Value}^^^&{cx.AssigningAuthority.UniversalID.Value}&ISO to alternate identifiers");
+					tracer.TraceEvent(TraceEventType.Information, 0, $"Adding {cx.ID.Value}^^^&{cx.AssigningAuthority.UniversalID.Value}&ISO to alternate identifiers");
 #endif
-					MessageUtil.tracer.TraceEvent(TraceEventType.Information, 0, $"Adding identifier from {cx.AssigningAuthority.UniversalID.Value} domain to alternate identifiers");
+					tracer.TraceEvent(TraceEventType.Information, 0, $"Adding identifier from {cx.AssigningAuthority.UniversalID.Value} domain to alternate identifiers");
 
 					entityIdentifier.Authority = assigningAuthority;
 					entityIdentifier.Value = cx.ID.Value;
@@ -277,22 +303,22 @@ namespace OpenIZ.Messaging.HL7
 		/// <returns>Returns a list of entity names.</returns>
 		public static IEnumerable<EntityName> ConvertNames(XPN[] names)
 		{
-			List<EntityName> entityNames = new List<EntityName>();
+			var entityNames = new List<EntityName>();
 
 			if (names.Length == 0)
 			{
 				return entityNames.AsEnumerable();
 			}
 
-			for (int i = 0; i < names.Length; i++)
+			for (var i = 0; i < names.Length; i++)
 			{
-				EntityName entityName = new EntityName();
+				var entityName = new EntityName();
 
-				Guid nameUse = NameUseKeys.Search;
+				var nameUse = NameUseKeys.Search;
 
 				if (!string.IsNullOrEmpty(names[i].NameTypeCode.Value) && !string.IsNullOrWhiteSpace(names[i].NameTypeCode.Value))
 				{
-					var concept = GetConcept(names[i].NameTypeCode.Value, "2.16.840.1.113883.5.1120");
+					var concept = GetConcept(names[i].NameTypeCode.Value, "urn:oid:2.16.840.1.113883.5.1120");
 
 					// TODO: cleanup
 					if (concept == null)
@@ -353,7 +379,7 @@ namespace OpenIZ.Messaging.HL7
 				return result;
 			}
 
-			object dateTime = null;
+			object dateTime;
 
 			if (Util.TryFromWireFormat(timestamp.TimeOfAnEvent.Value, typeof(MARC.Everest.DataTypes.TS), out dateTime))
 			{
@@ -362,27 +388,27 @@ namespace OpenIZ.Messaging.HL7
 			else
 			{
 #if DEBUG
-				MessageUtil.tracer.TraceEvent(TraceEventType.Warning, 0, string.Format("Unable to convert date of birth value: {0}", timestamp.TimeOfAnEvent.Value));
+				tracer.TraceEvent(TraceEventType.Warning, 0, $"Unable to convert date of birth value: {timestamp.TimeOfAnEvent.Value}");
 #endif
-				MessageUtil.tracer.TraceEvent(TraceEventType.Warning, 0, "Unable to convert date of birth value");
+				tracer.TraceEvent(TraceEventType.Warning, 0, "Unable to convert date of birth value");
 			}
 
 			return result;
 		}
 
 		/// <summary>
-		/// Converts a <see cref="NHapi.Model.V231.Datatype.XTN"/> instance to a <see cref="EntityTelecomAddress"/> instance.
+		/// Converts a <see cref="XTN"/> instance to a <see cref="EntityTelecomAddress"/> instance.
 		/// </summary>
 		/// <param name="xtn">The v2 XTN instance to be converted.</param>
 		/// <returns>Returns the converted entity telecom address instance.</returns>
 		public static EntityTelecomAddress ConvertXTN(XTN xtn)
 		{
-			Regex re = new Regex(@"([+0-9A-Za-z]{1,4})?\((\d{3})\)?(\d{3})\-(\d{4})X?(\d{1,6})?");
+			var re = new Regex(@"([+0-9A-Za-z]{1,4})?\((\d{3})\)?(\d{3})\-(\d{4})X?(\d{1,6})?");
 			var retVal = new EntityTelecomAddress();
 
 			if (xtn.AnyText.Value == null)
 			{
-				StringBuilder sb = new StringBuilder("tel:");
+				var sb = new StringBuilder("tel:");
 
 				try
 				{
@@ -444,7 +470,7 @@ namespace OpenIZ.Messaging.HL7
 
 			if (!string.IsNullOrEmpty(xtn.TelecommunicationUseCode.Value))
 			{
-				var concept = GetConcept(xtn.TelecommunicationUseCode.Value, "2.16.840.1.113883.5.1011");
+				var concept = GetConcept(xtn.TelecommunicationUseCode.Value, "urn:oid:2.16.840.1.113883.5.1011");
 
 				// TODO: cleanup
 				if (concept == null)
@@ -457,7 +483,6 @@ namespace OpenIZ.Messaging.HL7
 
 			retVal.AddressUseKey = use;
 
-			// Capability
 			return retVal;
 		}
 
@@ -471,21 +496,22 @@ namespace OpenIZ.Messaging.HL7
 		/// </summary>
 		public static IMessage CreateNack(IMessage request, List<IResultDetail> errors, Type errType)
 		{
-			var config = ApplicationContext.Current.Configuration;
+			var ack = errType.GetConstructor(Type.EmptyTypes).Invoke(null) as IMessage;
 
-			IMessage ack = errType.GetConstructor(Type.EmptyTypes).Invoke(null) as IMessage;
+			if (errors.Any())
+			{
+				tracer.TraceEvent(TraceEventType.Warning, 0, "Validation Errors:");
 
-			MessageUtil.tracer.TraceEvent(TraceEventType.Warning, 0, "Validation Errors:");
+				errors.ForEach(o => tracer.TraceEvent(TraceEventType.Error, 0, $"\t{o.Type} : {o.Message}"));
+			}
 
-			errors.ForEach(o => Trace.TraceError("\t{0} : {1}", o.Type, o.Message));
-
-			Terser terser = new Terser(ack);
+			var terser = new Terser(ack);
 
 			MessageUtil.UpdateMSH(terser, request);
 
-			int errLevel = 0;
+			var errLevel = 0;
 
-			int ec = 0;
+			var ec = 0;
 
 			foreach (var dtl in errors)
 			{
@@ -497,9 +523,9 @@ namespace OpenIZ.Messaging.HL7
 					else
 						errSeg = terser.getSegment(string.Format("/ERR", ec++));
 
-					if (errSeg is NHapi.Model.V231.Segment.ERR)
+					if (errSeg is ERR)
 					{
-						var tErr = MessageUtil.UpdateERR(errSeg as NHapi.Model.V231.Segment.ERR, dtl);
+						var tErr = UpdateERR(errSeg as ERR, dtl);
 
 						if (tErr > errLevel)
 						{
@@ -508,7 +534,7 @@ namespace OpenIZ.Messaging.HL7
 					}
 					else if (errSeg is NHapi.Model.V25.Segment.ERR)
 					{
-						var tErr = MessageUtil.UpdateERR(errSeg as ERR, dtl);
+						var tErr = UpdateERR(errSeg as ERR, dtl);
 
 						if (tErr > errLevel)
 						{
@@ -541,23 +567,32 @@ namespace OpenIZ.Messaging.HL7
 
 			if (request.Version == "2.3.1")
 			{
-				NHapi.Model.V231.Message.ACK ack = new NHapi.Model.V231.Message.ACK();
-				Terser terser = new Terser(ack);
+				var ack = new ACK();
+
+				var terser = new Terser(ack);
+
 				terser.Set("/MSA-1", responseCode);
 				terser.Set("/MSA-3", "Error occurred");
 				terser.Set("/MSA-6-1", errCode);
 				terser.Set("/MSA-6-2", errDescription);
-				MessageUtil.UpdateMSH(terser, request);
+
+				UpdateMSH(terser, request);
+
 				return ack;
 			}
 			else
 			{
-				NHapi.Model.V25.Message.ACK ack = new NHapi.Model.V25.Message.ACK();
-				Terser terser = new Terser(ack);
+				var ack = new NHapi.Model.V25.Message.ACK();
+
+				var terser = new Terser(ack);
+
 				terser.Set("/MSA-1", responseCode);
-				MessageUtil.UpdateMSH(terser, request);
+
+				UpdateMSH(terser, request);
+
 				terser.Set("/ERR-3-1", errCode);
 				terser.Set("/ERR-3-2", errDescription);
+
 				return ack;
 			}
 		}
@@ -573,55 +608,19 @@ namespace OpenIZ.Messaging.HL7
 		/// <returns>Returns the patient instance.</returns>
 		public static Patient CreatePatient(MSH msh, EVN evn, PID pid, PD1 pd1, List<IResultDetail> details)
 		{
-			Patient patient = new Patient();
-
-			patient.Addresses = MessageUtil.ConvertAddresses(pid.GetPatientAddress()).ToList();
-
-			var dateOfBirth = MessageUtil.ConvertTS(pid.DateTimeOfBirth);
-
-			if (dateOfBirth.HasValue)
+			var patient = new Patient
 			{
-				patient.DateOfBirth = dateOfBirth;
-			}
-			else
+				Addresses = ConvertAddresses(pid.GetPatientAddress()).ToList(),
+				DateOfBirth = ConvertTS(pid.DateTimeOfBirth),
+				DeceasedDate = ConvertTS(pid.PatientDeathDateAndTime),
+				GenderConceptKey = GetConcept(pid.Sex.Value, "urn:oid:2.16.840.1.113883.5.1")?.Key,
+				Names = ConvertNames(pid.GetPatientName()).ToList(),
+			};
+
+			if (patient.GenderConceptKey == null || (patient.GenderConceptKey.HasValue && patient.GenderConceptKey.Value == Guid.Empty))
 			{
 				details.Add(new MandatoryElementMissingResultDetail(ResultDetailType.Error, null, null));
-				MessageUtil.tracer.TraceEvent(TraceEventType.Warning, 0, "Patient doesn't have date of birth");
-			}
-
-			var dateOfDeath = MessageUtil.ConvertTS(pid.PatientDeathDateAndTime);
-
-			if (dateOfDeath.HasValue)
-			{
-				patient.DeceasedDate = dateOfDeath.Value;
-			}
-
-			try
-			{
-				if (!string.IsNullOrEmpty(pid.Sex.Value) && !string.IsNullOrWhiteSpace(pid.Sex.Value))
-				{
-					var concept = GetConcept(pid.Sex.Value, "2.16.840.1.113883.5.1");
-
-					// TODO: cleanup
-					if (concept == null)
-					{
-						throw new ArgumentException("Code not known");
-					}
-
-					patient.GenderConceptKey = concept.Key.Value;
-				}
-				else
-				{
-					details.Add(new MandatoryElementMissingResultDetail(ResultDetailType.Error, null, null));
-				}
-			}
-			catch (KeyNotFoundException)
-			{
 				details.Add(new NotSupportedChoiceResultDetail(ResultDetailType.Error, null, null));
-#if DEBUG
-				MessageUtil.tracer.TraceEvent(TraceEventType.Error, 0, $"Gender value {pid.Sex.Value} not found in map");
-#endif
-				MessageUtil.tracer.TraceEvent(TraceEventType.Error, 0, "Gender value not found in map");
 			}
 
 			patient.Identifiers.AddRange(MessageUtil.ConvertIdentifiers(pid.GetPatientIdentifierList()));
@@ -637,24 +636,19 @@ namespace OpenIZ.Messaging.HL7
 				patient.MultipleBirthOrder = Convert.ToInt32(pid.MultipleBirthIndicator.Value);
 			}
 
-			patient.Names = MessageUtil.ConvertNames(pid.GetPatientName()).ToList();
+			patient.Telecoms.AddRange(pid.GetPhoneNumberHome().Select(ConvertXTN));
+			patient.Telecoms.AddRange(pid.GetPhoneNumberBusiness().Select(ConvertXTN));
 
-			foreach (var item in pid.GetPhoneNumberHome())
+			if (pid.MotherSIdentifierRepetitionsUsed > 0 || pid.MotherSMaidenNameRepetitionsUsed > 0)
 			{
-				patient.Telecoms.Add(MessageUtil.ConvertXTN(item));
+				var person = new Person
+				{
+					Identifiers = ConvertIdentifiers(pid.GetMotherSIdentifier()).ToList(),
+					Names = ConvertNames(pid.GetMotherSMaidenName()).ToList(),
+				};
+
+				patient.Relationships.Add(new EntityRelationship(EntityRelationshipTypeKeys.Mother, person));
 			}
-
-			foreach (var item in pid.GetPhoneNumberBusiness())
-			{
-				patient.Telecoms.Add(MessageUtil.ConvertXTN(item));
-			}
-
-			var person = new Person();
-
-			person.Identifiers.AddRange(MessageUtil.ConvertIdentifiers(pid.GetMotherSIdentifier()));
-			person.Names.AddRange(MessageUtil.ConvertNames(pid.GetMotherSMaidenName()));
-
-			patient.Relationships.Add(new EntityRelationship(EntityRelationshipTypeKeys.Mother, person));
 
 			return patient;
 		}
@@ -673,7 +667,9 @@ namespace OpenIZ.Messaging.HL7
 		/// <exception cref="System.InvalidOperationException"></exception>
 		public static string GetCode(Guid conceptKey, Guid codeSystemKey)
 		{
-			var concept = conceptRepositoryService.GetConcept(conceptKey, Guid.Empty) as Concept;
+			var conceptService = ApplicationContext.Current.GetConceptService();
+
+			var concept = conceptService.GetConcept(conceptKey, Guid.Empty) as Concept;
 
 			if (concept == null)
 			{
@@ -696,6 +692,8 @@ namespace OpenIZ.Messaging.HL7
 		/// </exception>
 		public static Concept GetConcept(string code, string codeSystem)
 		{
+			var conceptService = ApplicationContext.Current.GetConceptService();
+
 			if (code == null)
 			{
 				throw new ArgumentNullException(nameof(code), "Value cannot be null");
@@ -706,7 +704,7 @@ namespace OpenIZ.Messaging.HL7
 				throw new ArgumentNullException(nameof(codeSystem), "Value cannot be null");
 			}
 
-			return conceptRepositoryService.FindConceptsByReferenceTerm(code, new Uri(codeSystem)).FirstOrDefault();
+			return conceptService.FindConceptsByReferenceTerm(code, new Uri(codeSystem)).FirstOrDefault();
 		}
 
 		/// <summary>
@@ -761,7 +759,7 @@ namespace OpenIZ.Messaging.HL7
 			err.Severity.Value = dtl.Type.ToString()[0].ToString();
 
 			// Determine the type of acknowledgement
-			string errCode = MapErrCode(dtl);
+			var errCode = MapErrCode(dtl);
 
 			err.HL7ErrorCode.Identifier.Value = errCode;
 			err.HL7ErrorCode.Text.Value = locale?.GetString($"HL7{errCode}");
@@ -871,7 +869,7 @@ namespace OpenIZ.Messaging.HL7
 		/// <returns>Returns a list of validation results.</returns>
 		public static List<IResultDetail> Validate(IMessage message, List<IResultDetail> details)
 		{
-			PipeParser pipeParser = new PipeParser() { ValidationContext = new DefaultValidation() };
+			var pipeParser = new PipeParser() { ValidationContext = new DefaultValidation() };
 
 			try
 			{
@@ -884,18 +882,35 @@ namespace OpenIZ.Messaging.HL7
 
 			try
 			{
-				Terser terser = new Terser(message);
+				var terser = new Terser(message);
 
-				var msh = terser.getSegment("MSH") as MSH;
+				var msh = terser.getSegment("MSH");
 
-				if (msh != null)
+				if (msh is MSH)
 				{
-					var result = MessageUtil.ConvertAssigningAuthority(msh.SendingApplication, details);
+					var v231Msh = (MSH)msh;
+
+					var result = ConvertAssigningAuthority(v231Msh.SendingApplication, details);
 
 					if (result == null)
 					{
-						details.Add(new UnrecognizedSenderResultDetail(ResultDetailType.Error, msh.SendingApplication.NamespaceID.Value, "MSH^3"));
+						details.Add(new UnrecognizedSenderResultDetail(ResultDetailType.Error, v231Msh.SendingApplication.NamespaceID.Value, "MSH^3"));
 					}
+				}
+				else if (msh is NHapi.Model.V25.Segment.MSH)
+				{
+					var v25Msh = (NHapi.Model.V25.Segment.MSH)msh;
+
+					var result = ConvertAssigningAuthority(v25Msh.SendingApplication, details);
+
+					if (result == null)
+					{
+						details.Add(new UnrecognizedSenderResultDetail(ResultDetailType.Error, v25Msh.SendingApplication.NamespaceID.Value, "MSH^3"));
+					}
+				}
+				else
+				{
+					details.Add(new MandatoryElementMissingResultDetail(ResultDetailType.Error, "Missing MSH", "MSH"));
 				}
 			}
 			catch (Exception e)
@@ -921,7 +936,6 @@ namespace OpenIZ.Messaging.HL7
 			if (match.Groups[1].Value != "tel:")
 			{
 				instance.AnyText.Value = tel.Value;
-				//instance.TelephoneNumber.Value = tel.Value;
 				return;
 			}
 
@@ -974,7 +988,7 @@ namespace OpenIZ.Messaging.HL7
 			// Tel use
 			if (tel.AddressUseKey != null)
 			{
-				var concept = GetConcept(instance.TelecommunicationUseCode.Value, "2.16.840.1.113883.5.1011");
+				var concept = GetConcept(instance.TelecommunicationUseCode.Value, "urn:oid:2.16.840.1.113883.5.1011");
 
 				// TODO: cleanup
 				if (concept == null)
@@ -993,15 +1007,15 @@ namespace OpenIZ.Messaging.HL7
 		/// <param name="instance">The converted XTN instance.</param>
 		public static void XTNFromTel(EntityTelecomAddress tel, NHapi.Model.V231.Datatype.XTN instance)
 		{
-			NHapi.Model.V25.Datatype.XTN v25instance = new NHapi.Model.V25.Datatype.XTN(instance.Message);
+			var v25Instance = new NHapi.Model.V25.Datatype.XTN(instance.Message);
 
-			MessageUtil.XTNFromTel(tel, v25instance);
+			XTNFromTel(tel, v25Instance);
 
-			for (int i = 0; i < v25instance.Components.Length; i++)
+			for (var i = 0; i < v25Instance.Components.Length; i++)
 			{
-				if (v25instance.Components[i] is AbstractPrimitive && i < instance.Components.Length)
+				if (v25Instance.Components[i] is AbstractPrimitive && i < instance.Components.Length)
 				{
-					(instance.Components[i] as AbstractPrimitive).Value = (v25instance.Components[i] as AbstractPrimitive).Value;
+					(instance.Components[i] as AbstractPrimitive).Value = (v25Instance.Components[i] as AbstractPrimitive).Value;
 				}
 			}
 		}
