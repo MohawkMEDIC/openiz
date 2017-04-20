6661a7490e8b864b0e581cc5ed47a0a1e5c0d51f
fyfej <fyfej@FYFEMASTER-2>
2017-04-19 20:25:13 -0400

    Updated care plan service

---
+++ OpenIZ.Core.Model/Acts/CarePlan.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using OpenIZ.Core.Model.Collection;
+using OpenIZ.Core.Model.Roles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OpenIZ.Core.Model.Acts
+{
+    /// <summary>
+    /// Represents a care plan
+    /// </summary>
+    [XmlType(nameof(CarePlan), Namespace = "http://openiz.org/model")]
+    [XmlRoot(nameof(CarePlan), Namespace = "http://openiz.org/model")]
+    [JsonObject(nameof(CarePlan))]
+    public class CarePlan : Bundle
+    {
+
+        /// <summary>
+        /// Create a care plan request
+        /// </summary>
+        public static CarePlan CreateCarePlanRequest(Patient p)
+        {
+            return new CarePlan() { Item = new List<IdentifiedData>() { p });
+        }
+    }
+}
