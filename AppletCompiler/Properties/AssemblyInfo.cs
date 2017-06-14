343b7229ef5f336c629fef54ede4a1f29df6d65e
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-06-14 00:18:47 -0400

    version number update

--- AppletCompiler/Properties/AssemblyInfo.cs
+++ AppletCompiler/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.8.0.*")][assembly: AssemblyInformationalVersion("Chippewa")]
-[assembly: AssemblyVersion("0.8.0.*")][assembly: AssemblyInformationalVersion("Dalhouse")]
+// [assembly: AssemblyVersion("0.9.4.*")][assembly: AssemblyInformationalVersion("Chippewa")]
+[assembly: AssemblyVersion("0.9.4.*")][assembly: AssemblyInformationalVersion("Dalhouse")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- OpenIZ.Messaging.HL7.Test/Properties/AssemblyInfo.cs
+++ OpenIZ.Messaging.HL7.Test/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.8.0.*")][assembly: AssemblyInformationalVersion("Chippewa")]
-[assembly: AssemblyVersion("0.8.0.*")][assembly: AssemblyInformationalVersion("Chippewa")]
+// [assembly: AssemblyVersion("0.9.4.*")][assembly: AssemblyInformationalVersion("Chippewa")]
+[assembly: AssemblyVersion("0.9.4.*")][assembly: AssemblyInformationalVersion("Chippewa")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
