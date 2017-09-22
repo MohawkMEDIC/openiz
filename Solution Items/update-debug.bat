b78ab45a002f269341ff308f91346b55f4b7a50a
fyfej <fyfej@FYFEJ-PC>
2017-09-22 12:42:37 -0400

    Updated the DLLS from service core, updated the indexing, and updated the care plan warehouse

--- Solution Items/update-debug.bat
+++ Solution Items/update-debug.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 for %%d in (*.dll) do (
 		echo %%d
-               copy ..\..\medicsvccore\bin\debug\%%d .\
+               copy ..\..\servicecore\bin\debug\%%d .\
 )
 for %%d in (*.pdb) do (
 		echo %%d
