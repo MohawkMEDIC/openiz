0a72e5dc529ad8697e3bf8a2ac434eeb881ffd53
fyfej <fyfej@FYFEJ-PC>
2017-10-04 16:30:42 -0400

    Added join filter attribute

---
+++ OpenIZ.OrmLite/Attributes/JoinFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIZ.OrmLite.Attributes
+{
+    /// <summary>
+    /// Join filter attribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class JoinFilterAttribute : Attribute
+    {
+
+        /// <summary>
+        /// The field on which to filter
+        /// </summary>
+        public String PropertyName { get; set; }
+
+        /// <summary>
+        /// The constant to filter on
+        /// </summary>
+        public String Value { get; set; }
+    }
+}
