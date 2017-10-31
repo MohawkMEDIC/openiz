615a2dce9d6daedb70393e919aa98206d7b749d3
justin-fyfe <justin.fyfe1@mohawkcollege.ca>
2017-10-31 19:23:28 -0400

    Updated script references to be backward compatible

---
+++ OpenIZ.Core.Applets/Model/AssetScriptReference.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace OpenIZ.Core.Applets.Model
+{
+    /// <summary>
+    /// Represents an asset script reference
+    /// </summary>
+    [XmlType(nameof(AssetScriptReference), Namespace = "http://openiz.org/applet")]
+    [JsonObject(nameof(AssetScriptReference))]
+    public class AssetScriptReference
+    {
+        /// <summary>
+        /// True if the reference is static
+        /// </summary>
+        [XmlAttribute("static")]
+        public bool IsStatic { get; set; }
+
+        /// <summary>
+        /// Reference itself
+        /// </summary>
+        [XmlText]
+        [JsonProperty("href")]
+        public string Reference { get; set; }
+
+    }
+}
